4736d8f4a73b111fc8eb9041a21ad079756a3f00
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-02-16 23:08:38 +0530

    swagger config

--- src/main/java/com/vpcodelabs/springboot/mongodb/controller/PersonController.java
+++ src/main/java/com/vpcodelabs/springboot/mongodb/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.vpcodelabs.springboot.mongodb.controller;
 
 import com.vpcodelabs.springboot.mongodb.collection.Person;
 import com.vpcodelabs.springboot.mongodb.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/person")
+@Tag(name = "person-controllers")
 public class PersonController {
 
     private final PersonService personService;
 
     @PostMapping
+    @Operation(summary = "create person", description = "record a new person details by providing the necessary details.")
     public String save(@RequestBody Person person){
         return personService.save(person);
     }
