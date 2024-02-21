ea1446ddc1fceda4f0ec86fcc8aaaea4ec28a59a
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-02-21 23:34:05 +0530

    feature aggregation implemented

--- src/main/java/com/vpcodelabs/springboot/mongodb/service/PersonServiceImpl.java
+++ src/main/java/com/vpcodelabs/springboot/mongodb/service/PersonServiceImpl.java
@@ -3,9 +3,12 @@ package com.vpcodelabs.springboot.mongodb.service;
 import com.vpcodelabs.springboot.mongodb.collection.Person;
 import com.vpcodelabs.springboot.mongodb.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -62,11 +65,36 @@ public class PersonServiceImpl implements PersonService {
                     .andOperator(criteria.toArray(new Criteria[0])));
         }
 
-        Page<Person> people = PageableExecutionUtils.getPage(
+        return PageableExecutionUtils.getPage(
                 mongoTemplate.find(query, Person.class),
-                        pageable,
-                        () -> mongoTemplate.count(query.skip(0).limit(0), Person.class)
+                pageable,
+                () -> mongoTemplate.count(query.skip(0).limit(0), Person.class)
         );
-        return people;
+    }
+
+    @Override
+    public List<Document> getOldestPersonByCity() {
+        UnwindOperation unwindOperation = Aggregation.unwind("addresses");
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC, "age");
+        GroupOperation groupOperation = Aggregation.group("addresses.city").first(Aggregation.ROOT).as("oldestPerson");
+
+        Aggregation aggregation = Aggregation.newAggregation(unwindOperation, sortOperation, groupOperation);
+
+        return mongoTemplate.aggregate(aggregation, Person.class, Document.class).getMappedResults();
+    }
+
+    @Override
+    public List<Document> getPopulationByCity() {
+        UnwindOperation unwindOperation = Aggregation.unwind("addresses");
+        GroupOperation groupOperation = Aggregation.group("addresses.city").count().as("popCount");
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC, "popCount");
+
+        ProjectionOperation projectionOperation = Aggregation.project()
+                .andExpression("_id").as("city")
+                .andExpression("popCount").as("count")
+                .andExclude("_id");
+        Aggregation aggregation = Aggregation.newAggregation(unwindOperation, groupOperation, sortOperation, projectionOperation);
+
+        return mongoTemplate.aggregate(aggregation, Person.class, Document.class).getMappedResults();
     }
 }
